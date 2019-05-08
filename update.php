d6a99225155e32740b2cd81fed9485fab8ec7024
smashhr <meeeeebo9@gmail.com>
2019-05-08 08:27:01 +0400

    initial commit

--- update.php
+++ update.php
@@ -21,7 +21,7 @@ include ('getConnection.php');
 
 
 	//$sql = "UPDATE instructors SET un='$un' ,  pw= $pw  ,  nam='$nam' , gen='$gen'  ,  num=$num ,  age=$age ,  ex=$ex , dt='$dt' where un='$un' and  pw= $pw";
-	$sql = "UPDATE instructors SET un=$_POST['un'] ,  pw= $_POST['nam']  ,  nam=$_POST['nam'] , gen=$_POST['gen']  ,  num=$_POST['num'] ,  age=$_POST['age'] ,  ex=$_POST['ex'] , dt=$_POST['dt'] where un='$un' and  pw= $pw";
+	$sql = "UPDATE instructors SET un=$_POST['un'] ,  pw= $_POST['nam']  ,  nam=$_POST['nam'] , gen=$_POST['gen']  ,  num=$_POST['num'] ,  age=$_POST['age'] ,  ex=$_POST['ex'] , dt=$_POST['dt'] where un='$un' and  pw= '$pw' ";
 
 	
 	if ($cn->query($sql)===TRUE) 
